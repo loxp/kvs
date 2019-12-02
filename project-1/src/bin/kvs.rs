ec86f01c1f64b0359c9e36f4b2fd6631bcdf91f2
loxp <loxppqqxol@gmail.com>
2019-12-02 21:55:00 +0800

    add cmd line bin file

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,1 +1,42 @@
-fn main() {}
+use clap::{Arg, App, SubCommand, AppSettings};
+use std::process::exit;
+
+fn main() {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::DisableHelpSubcommand)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::VersionlessSubcommands)
+        .subcommand(
+            SubCommand::with_name("set")
+                .arg(Arg::with_name("KEY").required(true))
+                .arg(Arg::with_name("VALUE").required(true))
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .arg(Arg::with_name("KEY").required(true))
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .arg(Arg::with_name("KEY").required(true))
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("set", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("get", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("rm", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _ => unreachable!(),
+    }
+}
